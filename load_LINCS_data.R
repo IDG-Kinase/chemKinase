83244afb27107b4581acc8b20c5095566c76b272
Matthew Berginski <matthew.berginski@unc.edu>
2018-03-07 12:59:09 -0500

    updating column names in LINCS data processing script

--- load_LINCS_data.R
+++ load_LINCS_data.R
@@ -1,17 +1,17 @@
 library(BerginskiRMisc)
 library(here)
+library(tidyverse)
 
 LINCS_KINOMEscan_Kd = read.csv.all(here('LINCS/Data Files/Kd/*'));
 
 LINCS_KINOMEscan_Kd_noNA = LINCS_KINOMEscan_Kd %>%
   filter(!is.na(Kd))
 
-write.csv(LINCS_KINOMEscan_Kd,'LINCS_KINOMEscan_Kd.csv', row.names = F)
-write.csv(LINCS_KINOMEscan_Kd_noNA,'LINCS_KINOMEscan_Kd_noNA.csv', row.names = F)
+write.csv(LINCS_KINOMEscan_Kd,here('LINCS_KINOMEscan_Kd.csv'), row.names = F)
+write.csv(LINCS_KINOMEscan_Kd_noNA,here('LINCS_KINOMEscan_Kd_noNA.csv'), row.names = F)
 
 LINCS_KINOMEscan_percent = read.csv.all(here('LINCS/Data Files/percent_control/*'));
+LINCS_KINOMEscan_percent = LINCS_KINOMEscan_percent %>%
+  rename(Percent.Control=X..Control)
 
-LINCS_KINOMEscan_percent_noNA = LINCS_KINOMEscan_percent
-
-write.csv(LINCS_KINOMEscan_percent,'LINCS_KINOMEscan_percent.csv', row.names = F)
-write.csv(LINCS_KINOMEscan_percent_noNA,'LINCS_KINOMEscan_percent_noNA.csv', row.names = F)
+write.csv(LINCS_KINOMEscan_percent,here('LINCS_KINOMEscan_percent.csv'), row.names = F)
