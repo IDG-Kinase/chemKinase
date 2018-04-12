c23d07ddf6201ce209d1bc15568240bb3eef939b
Matthew Berginski <matthew.berginski@unc.edu>
2018-04-12 13:40:38 -0400

    added loading of second UNC data set, blocked extra csv writing

--- load_all_data.R
+++ load_all_data.R
@@ -12,16 +12,16 @@ LINCS_KINOMEscan_Kd = read.csv.all(here('LINCS/Data Files/Kd/*'));
 LINCS_KINOMEscan_Kd_noNA = LINCS_KINOMEscan_Kd %>%
   filter(!is.na(Kd))
 
-write.csv(LINCS_KINOMEscan_Kd,here('LINCS_KINOMEscan_Kd.csv'), row.names = F)
-write.csv(LINCS_KINOMEscan_Kd_noNA,here('LINCS_KINOMEscan_Kd_noNA.csv'), row.names = F)
+# write.csv(LINCS_KINOMEscan_Kd,here('LINCS_KINOMEscan_Kd.csv'), row.names = F)
+# write.csv(LINCS_KINOMEscan_Kd_noNA,here('LINCS_KINOMEscan_Kd_noNA.csv'), row.names = F)
 
 LINCS_KINOMEscan_percent = read.csv.all(here('LINCS/Data Files/percent_control/*'));
 LINCS_KINOMEscan_percent = LINCS_KINOMEscan_percent %>%
   rename(Percent.Control=X..Control) %>%
   select(-one_of('Small.Molecule.HMS.LINCS.ID','Protein.HMS.LINCS.ID'))
 
-write.csv(LINCS_KINOMEscan_percent,here('LINCS_KINOMEscan_percent.csv'), row.names = F)
-saveRDS(LINCS_KINOMEscan_percent,here('LINCS_KINOMEscan_percent.rds'))
+# write.csv(LINCS_KINOMEscan_percent,here('LINCS_KINOMEscan_percent.csv'), row.names = F)
+# saveRDS(LINCS_KINOMEscan_percent,here('LINCS_KINOMEscan_percent.rds'))
 
 UNC_KINOMEscan_percent = read_excel(here('UNC/journal.pone.0181585.s004.xlsx'))
 UNC_KINOMEscan_percent = UNC_KINOMEscan_percent %>% 
@@ -31,13 +31,24 @@ UNC_KINOMEscan_percent = UNC_KINOMEscan_percent %>%
   mutate(Assay.compound.conc = 1, Conc.unit = 'uM') %>%
   filter(!is.na(Small.Molecule.Name))
 
-write.csv(LINCS_KINOMEscan_percent,here('UNC_KINOMEscan_percent.csv'), row.names = F)
-saveRDS(LINCS_KINOMEscan_percent,here('UNC_KINOMEscan_percent.rds'))
+UNC_KINOMEscan_percent_2 = read_csv(here('UNC/SGC024-01-p-00003_Data Report.csv')) %>%
+  rename(Small.Molecule.Name = "Compound Name", 
+         Protein.Name = "DiscoveRx Gene Symbol",
+         Percent.Control = "Percent Control",
+         Assay.compound.conc = "Compound Concentration (nM)") %>%
+  select(-`Entrez Gene Symbol`) %>%
+  mutate(Conc.unit = "nM")
+
+# write.csv(LINCS_KINOMEscan_percent,here('UNC_KINOMEscan_percent.csv'), row.names = F)
+
+
+
+# saveRDS(LINCS_KINOMEscan_percent,here('UNC_KINOMEscan_percent.rds'))
 
 ###############################################################################
 # Glue Data Sets Together and Save
 ###############################################################################
 
-full_percent_set = rbind(LINCS_KINOMEscan_percent,UNC_KINOMEscan_percent)
+full_percent_set = rbind(LINCS_KINOMEscan_percent,UNC_KINOMEscan_percent, UNC_KINOMEscan_percent_2)
 
 saveRDS(full_percent_set,here('shiny','data','Full_KINOMEscan_percent.rds'))
